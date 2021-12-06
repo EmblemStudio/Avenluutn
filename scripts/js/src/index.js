cedbd1e07325014e437f869e878486a287ba1219
Jesse B. Miller <jesse@jessebmiller.com>
2021-12-06 14:43:45 -0600

    script improvements

--- scripts/js/src/index.js
+++ scripts/js/src/index.js
@@ -1,4 +1,14 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.tellStories = void 0;
 const prando_1 = require("prando");
@@ -7,6 +17,7 @@ const tellStory_1 = require("./tellStory");
 const nextState_1 = require("./nextState");
 const cross_fetch_1 = require("cross-fetch");
 globalThis.fetch = cross_fetch_1.fetch;
+__exportStar(require("./content/interfaces"), exports);
 async function tellStories(prevResult, startTime, length, totalStories, providerUrl) {
     const provider = (0, utils_1.makeProvider)(providerUrl);
     const startBlockHash = await (0, utils_1.nextBlockHash)(startTime, provider);
