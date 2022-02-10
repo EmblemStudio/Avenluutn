691ec687aa2f86c3ff380d7df7376d52993f3f62
Jesse B. Miller <jesse@jessebmiller.com>
2022-02-10 15:06:19 -0600

    deployment

--- hardhat/scripts/newNarrator.js
+++ hardhat/scripts/newNarrator.js
@@ -19,6 +19,7 @@ async function main() {
   /**
    * mint narratorNFT
    */
+  /*
   const narratorTx = await narratorNFTs.mint(
     "0x9b8d5AF3625d81bb3376916c4D98A20B98b85bCF", // Squad Test
     "https://gist.githubusercontent.com/EzraWeller/4a7468ec46ee6b83ed0b167c24339f4c/raw/04edc8559415d8067c0d3c51abe6d9cc8d9203a3/avenluutn_bundle_090122.js"
@@ -28,10 +29,15 @@ async function main() {
   await narratorTx.wait()
 
   console.log("minted NFT")
-
+  */
   const minutes = 60
   const hours = minutes * 60
-  const start = 1641985200
+  const start = 1644534000
+  /*
+  Unix Timestamp	1644534000
+  GMT  Thu Feb 10 2022 23:00:00 GMT+0000
+  Your Time Zone	Thu Feb 10 2022 17:00:00 GMT-0600 (Central Standard Time)
+  */
         /* Unix Timestamp   1641985200
            GMT	            Wed Jan 12 2022 11:00:00 GMT+0000
            Your Time Zone   Wed Jan 12 2022 05:00:00 GMT-0600 (Central Standard Time)
@@ -39,11 +45,11 @@ async function main() {
 
   const now = parseInt((new Date().getTime() / 1000).toFixed(0))
   const pubTx = await publisher.addNarrator(
-    narratorNFTs.address,
-    Number(nftId),
+    "0x33A9d7DDB8f76B0c940010643B8774eDb1CB42e6", // narratorNFTs.address,
+    26, //Number(nftId),
     start,            // start
     60,               // totalCollections
-    11 * hours,       // collectionLength
+    11 * hours + 30 * minutes,       // collectionLength
     12 * hours,       // collectionSpacing
     5,                // collectionSize
   )
--- react/src/constants.ts
+++ react/src/constants.ts
@@ -8,7 +8,7 @@ interface NarratorParams {
 
 export const NARRATOR_PARAMS: NarratorParams = {
   network: "goerli",
-  narratorIndex: 15,
+  narratorIndex: 0,
 }
 
 export const NETWORK_IDS: { [key in NetworkName]: number } = {
@@ -33,7 +33,7 @@ export const STATUS = {
 export const ADDRESSES: { [name: string]: string } = {
   "mainnet": "",
   "ropsten": "0x2A7b3033c100044178E7c7FDdC939Be660178458",
-  "goerli": "0x0F28D67D68b2904fC0A0B76FB0f5d4980199E7c4",
+  "goerli": "0x6bb7758DB5b475B4208A5735A8023fdEdD753aaf",
   "polygon": "",
   "localhost": "0x720472c8ce72c2A2D711333e064ABD3E6BbEAdd3",
 }
