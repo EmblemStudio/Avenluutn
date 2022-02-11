824dc3e28647f0aec1e65373eba03acba2defd76
Jesse B. Miller <jesse@jessebmiller.com>
2022-02-11 11:43:41 -0600

    fix json marshal hints

--- echo-token-metadata/types.go
+++ echo-token-metadata/types.go
@@ -3,46 +3,46 @@ package main
 // TODO consider chaning display type and trait type to Enums
 // TODO consider doing attribute value type checking somehow
 type Attribute struct {
-	DisplayType string `json:display_type,omitempty`
-	TraitType string `json:trait_type`
-	Value interface{} `json:value`
+	DisplayType string `json:"display_type,omitempty"`
+	TraitType string `json:"trait_type"`
+	Value interface{} `json:"value"`
 }
 
 type StoryMeta struct {
-	Name string `json:name`
-	Description string `json:description`
-	Image string `json:image`
-	AnimationURL string `json:animation_url`
-	ExternalURL string `json:external_url`
-	Attributes []Attribute `json:attributes`
+	Name string `json:"name"`
+	Description string `json:"description"`
+	Image string `json:"image"`
+	AnimationURL string `json:"animation_url"`
+	ExternalURL string `json:"external_url"`
+	Attributes []Attribute `json:"attributes"`
 }
 
 type MiddlePart struct {
-	OutcomeText []OutcomePart `json:outcomeText`
+	OutcomeText []OutcomePart `json:"outcomeText"`
 }
 
 type OutcomePart struct {
-	ResultTexts [][]TextPart `json:resultTexts`
-	TriggerTexts [][]TextPart `json:resultTexts`
+	ResultTexts [][]TextPart `json:"resultTexts"`
+	TriggerTexts [][]TextPart `json:"resultTexts"`
 }
 
 type EndingPart struct {
-	ResultTexts [][]TextPart `json:resultTexts`
+	ResultTexts [][]TextPart `json:"resultTexts"`
 }
 
 type TextPart struct {
-	Label string `json:string`
-	String string `json:string`
+	Label string `json:"string"`
+	String string `json:"string"`
 }
 
 type StoryParts struct {
-	Middle MiddlePart `json:middle`
-	Ending EndingPart `json:ending`
-	Beginning []TextPart `json:beginning`
+	Middle MiddlePart `json:"middle"`
+	Ending EndingPart `json:"ending"`
+	Beginning []TextPart `json:"beginning"`
 }
 
 type Story struct {
-	RichText StoryParts `json:richText`
+	RichText StoryParts `json:"richText"`
 }
 
 type StoryInfo struct {
